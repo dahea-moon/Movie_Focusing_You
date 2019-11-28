4d2a6819744b5c1f4130f0ecbadced8baf044b2e
J. Kim <514kim@gmail.com>
2019-11-29 01:56:14 +0900

    edit few

--- movie-front/src/views/Mypage.vue
+++ movie-front/src/views/Mypage.vue
@@ -1,7 +1,26 @@
 <template>
   <div>
-      my page
-      {{ getUserDetail }}
+      <h1>{{ getUserDetail.username }}</h1>
+      <br>
+      <dl class="row">
+            <dt class="col-sm-6">1번째 선호 키워드</dt>
+            <dd class="col-sm-6">{{ getUserDetail.k1_content }}</dd>
+
+            <dt class="col-sm-6">2번째 선호 키워드</dt>
+            <dd class="col-sm-6">{{ getUserDetail.k2_content }}</dd>
+            
+            <dt class="col-sm-6">1번째 선호 장르</dt>
+            <dd class="col-sm-6">{{ getUserDetail.g1_name }}</dd>
+            
+            <dt class="col-sm-6">2번째 선호 장르</dt>
+            <dd class="col-sm-6">{{ getUserDetail.g2_name }}</dd>
+            
+            <dt class="col-sm-6">보고 싶은 영화 목록</dt>
+            <dd class="col-sm-6">{{ getUserDetail.wishlist }}</dd>
+            <dt class="col-sm-6">내가 봤던 영화 목록</dt>
+            <dd class="col-sm-6">{{ getUserDetail.watchedlist }}</dd>
+        </dl>
+
   </div>
 </template>
 
